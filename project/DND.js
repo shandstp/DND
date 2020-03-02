eff66004cbf50750617702e376805998f6aedef1
Travis Shands <shandstp@gmail.com>
2020-03-01 16:30:35 -0800

    Dropdown filter not quite working

--- project/DND.js
+++ project/DND.js
@@ -16,8 +16,19 @@ app.set('port', 7701);
 
 var context = {};
 
+function getGames(res, mysql, context, complete){
+  mysql.pool.query("SELECT gameID AS id, name FROM Games", function(error, results, fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.Game = results;
+    complete();
+  });
+}
+
 function getPlayers(res, mysql, context, complete){
-  mysql.pool.query("SELECT firstName, lastName, playableChar AS playable, Games.name AS game, class, SpouseId, hitpointVal, humanoidID FROM Humanoids INNER JOIN Games On Humanoids.gameID = Games.gameID", function(error, result, fields){
+  mysql.pool.query("SELECT firstName, lastName, playableChar AS playable, Games.name AS game, class, SpouseId, hitpointVal, humanoidID FROM Humanoids INNER JOIN Games ON Humanoids.gameID = Games.gameID", function(error, result, fields){
     if(error){
       res.write(JSON.stringify(error));
       res.end();
@@ -26,6 +37,21 @@ function getPlayers(res, mysql, context, complete){
     // console.log(result);
     complete();
   });
+}
+
+function getPlayersByGame(req, res, mysql, context, complete){
+      var query = "SELECT firstName, lastName, playableChar AS playable, Games.name AS game, class, SpouseId, hitpointVal, humanoidID FROM Humanoids INNER JOIN Games ON Humanoids.gameID = Games.gameID WHERE Humanoids.gameID = ?";
+      console.log(req.params)
+      var inserts = [req.params.gameid]
+      mysql.pool.query(query, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.player = results;
+            console.log(result);
+            complete();
+      });
 }
 
 function getDMs(res, mysql, context, complete){
@@ -63,13 +89,30 @@ app.use('/Players', function(req,res){
    var context = {};
    var mysql = req.app.get('mysql');
    getPlayers(res, mysql, context, complete);
+   getGames(res, mysql, context, complete);
    function complete(){
       callbackCount++;
-         if(callbackCount >= 1){
+         if(callbackCount >= 2){
             res.render('players', context);
          }
    }
    return;
+});
+
+app.get('/filter/:gameid', function(req, res){
+    var callbackCount = 0;
+    console.log("hitting filter route")
+    var context = {};
+    var mysql = req.app.get('mysql');
+    getPlayersByGame(res, mysql, context, complete);
+    getGames(res, mysql, context, complete);
+    function complete(){
+        callbackCount++;
+        if(callbackCount >= 2){
+            res.render('people', context);
+        }
+
+    }
 });
 
 app.use('/DungeonMasters', function(req,res){
