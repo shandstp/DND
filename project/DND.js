294a1e44e6156080f84c41cce1a1aa2c65b9e59a
Travis Shands <shandstp@gmail.com>
2020-03-02 13:00:39 -0800

    character insertions working

--- project/DND.js
+++ project/DND.js
@@ -12,9 +12,20 @@ app.use(express.static('views/images'));
 app.engine('handlebars', handlebars.engine);
 app.set('mysql', mysql)
 app.set('view engine', 'handlebars');
-app.set('port', 7701);
+app.set('port', 7017);
 
 var context = {};
+
+function getGames(res, mysql, context, complete){
+  mysql.pool.query("SELECT gameID AS id, name FROM Games", function(error, results, fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.Game = results;
+    complete();
+  });
+}
 
 function getPlayers(res, mysql, context, complete){
   mysql.pool.query("SELECT firstName, lastName, playableChar AS playable, Games.name AS game, class, SpouseId, hitpointVal, humanoidID FROM Humanoids INNER JOIN Games On Humanoids.gameID = Games.gameID", function(error, result, fields){
@@ -58,18 +69,39 @@ app.get('/', function(req,res){
    return;
 });
 
-app.use('/Players', function(req,res){
+app.get('/Players', function(req,res){
    var callbackCount = 0;
    var context = {};
    var mysql = req.app.get('mysql');
    getPlayers(res, mysql, context, complete);
+   getGames(res, mysql, context, complete);
    function complete(){
       callbackCount++;
-         if(callbackCount >= 1){
+         if(callbackCount >= 2){
             res.render('players', context);
          }
    }
-   return;
+   // return;
+});
+
+app.post('/Players', function(req, res){
+  console.log("test output");
+  // console.log(req.body.gameName);
+  console.log(req.body);
+  var mysql = req.app.get('mysql');
+  var sql = "INSERT INTO Humanoids (firstName, lastName, playableChar, gameID, class, hitpointVal) VALUES (?,?,1,?,?,20)";
+  var inserts = [req.body.firstname, req.body.lastname, req.body.gameName, req.body.class];
+  sql = mysql.pool.query(sql,inserts,function(error,results,fields){
+    if(error){
+      console.log(JSON.stringify(error));
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    else{
+      res.redirect('/Players');
+    }
+
+  });
 });
 
 app.use('/DungeonMasters', function(req,res){
